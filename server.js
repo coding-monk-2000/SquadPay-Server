115cfa5d06bc76425efed3d6384ba527b490c236
Richie Burns <coding.monk.2000@gmail.com>
2025-06-24 21:32:45 +0100

    add init graphql

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import "./src/config.js";
-
+import jwt from "jsonwebtoken";
 import express from "express";
 import router from "./src/routes/authRoutes.js";
 import db from "./src/db/db.js"
+import {ApolloServer} from "@apollo/server"
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from 'cors'
+import {typeDefs} from "./src/graphql/schema.js"
+import {resolvers} from "./src/graphql/resolvers.js"
 
 const app = express();
 
@@ -14,6 +19,38 @@ app.use((req, _res, next) => {
 });
 
 app.use("/api/auth", router);
+
+async function getUserFromToken(token) {
+    try {
+      if (!token) throw new Error("Missing JWT");
+      console.log(jwt.verify(token, process.env.SECRET_KEY))
+      return jwt.verify(token, process.env.SECRET_KEY);
+    } catch (error) {
+      throw new Error("Invalid JWT");
+    }
+  }
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+
+  app.use(
+    '/graphql', cors(),
+    expressMiddleware(server , {
+        context: async ({req}) => {
+            try {
+              const token = req.headers.authorization?.replace("Bearer ", "");
+              const {userId} = jwt.verify(token, process.env.SECRET_KEY)
+              return { userId, db};
+            } catch (error) {
+              throw new Error("Unauthorized");
+            }
+        }
+    })
+  )
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
